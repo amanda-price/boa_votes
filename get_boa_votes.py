122a17a12c4571561cab6e7a705e7233e6c4de84
amanda-price <A.price.138@gmail.com>
2019-05-05 20:29:59 -0500

    added date of vote

--- get_boa_votes.py
+++ get_boa_votes.py
@@ -18,13 +18,15 @@ def getVotes(bbid):
 			row = line.split()
 			bb = row[3]
 			print(year,bb)
-		#print(":: {}".format(line))
 		if line.startswith("Your feedback was not sent"):
 			writeOnFlag =False
-
+		if "/20" in line:
+			date = line
 		if writeOnFlag:
 			row = line.strip()
 			if not line.startswith("Alderman") and not noVoteFlag:
+				## We could ignore everything beginning with "Ward" but if the ward is vacant, theres a "Ward XX Vacant"
+				## listing in the absent section. 
 				if line.startswith("Ward") and not line.endswith("Vacant"):
 					continue
 				if not row == "":
@@ -36,11 +38,9 @@ def getVotes(bbid):
 							voteType = "Aye"
 						if voteType == "Noes":
 							voteType = "No"
-						vote_file.write("{},{},{},{}\n".format(bb,ward,alderperson,voteType))
+						vote_file.write("{},{},{},{},{}\n".format(bb,date,ward,alderperson,voteType))
 					count = count -1
-					
-				#print(row)
-		
+
 		if line.startswith(voteTypes):
 			for elem in voteTypes:
 				if line.startswith(elem):
@@ -53,24 +53,35 @@ def getVotes(bbid):
 			if numberOfVotes == 0:
 				noVoteFlag = True
 			else:
+				## We need the count multiplied by two, to parse for each ward/alderperson combo. There probably is 
+				## a better way to do this, but it works.
 				count = numberOfVotes*2
 				noVoteFlag = False
 
 
 bblist = []
+
+## These are the years shown currently. Once the 19/20 session starts, this could be replaced just to parse that 
+## new session.
 years = ["2018-2019","2017-2018","2016-2017","2015-2016"]
-#years = ["2016-2017","2015-2016"]
+
 
 for year in years:
+
+	## Separating the BOA votes into files by session
 	vote_file = open("boa_votes_"+year[2:4]+year[-2:]+".csv",'w')
-	vote_file.write("bbid,ward,alderperson,vote\n")
+	## bbid is the Board Bill number
+	vote_file.write("bbid,date of vote,ward,alderperson,vote\n")
+
 	url = "https://www.stlouis-mo.gov/government/city-laws/board-bills/votes/index.cfm?session="+year+"&submit=Choose+Session"
 	response = urllib.request.urlopen(url)
 	soup = BeautifulSoup(response.read(), 'html.parser')
 
+	## Unfortunately,the links are not tied to the exact board bill number (but man it would have been great if 
+	## they were). Instead, we need to parse the page for the links to the new pages
 	for link in soup.findAll('a', attrs={'href': re.compile("board-bill.cfm")}):
 		url_string = link.get('href')
-		if not url_string[-5] == "#":
+		if not url_string[-5] == "#": ## Ignore the links to the Aye and Nay votes, we want the full page
 			bblist.append(link.get('href'))
 
 	for bbid in bblist:
